3bc47b6f3e615ae6358a5a44ecf19bd4200cdbd3
Navya Rajput <navyasrajput@gmail.com>
2021-09-29 18:44:26 -0400

    Implement Logout Client & Server logic

--- express_server.js
+++ express_server.js
@@ -94,6 +94,12 @@ app.post("/login", (req, res) => {
   res.cookie('username', username);
   res.redirect('/urls');
 });
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
